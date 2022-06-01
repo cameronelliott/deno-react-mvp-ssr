e5c9d310943cff6a73d0873ff778440e4c0f4cc2
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-06-01 16:50:07 -0700

    react deps

---
+++ dep.ts
@@ -0,0 +1,11 @@
+
+import React from "https://esm.sh/react@18.1.0";
+
+import ReactDOM from "https://esm.sh/react-dom@18.1.0";
+
+import ReactDOMServer from "https://esm.sh/react-dom@18.1.0/server";
+
+
+export { React };
+export { ReactDOM };
+export { ReactDOMServer };
