0b0b0d4060fd1b9d3be9344e85231ad598f7ee1c
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-06-01 16:50:53 -0700

    mvp html for SSR react app on deno

---
+++ server.tsx
@@ -0,0 +1,34 @@
+// frontend/ssr/server.tsx
+
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+//import * as oak from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+import { React, ReactDOMServer } from "./dep.ts";
+import App from "./App.tsx";
+
+const app = new Application();
+const port = 8000;
+
+const router = new Router();
+router.get("/", (context) => {
+  context.response.body = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <title>Sanity <-> Deno</title>
+</head>
+<body >
+    <div id="root">${ReactDOMServer.renderToString(<App />)}
+    </div>
+</body>
+</html>`;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port });
+console.log(`server is running on port: ${port}`);
